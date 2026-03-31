f2952f3f3cf6b1c2a567e8fa0c4153203a99a253
P-Kuszyk <p.kuszyk1221@wp.pl>
2026-03-31 14:30:06 +0200

    Add users endpoint

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
 
+//#region GET
 app.get('/', (req, res) => {
     res.send("API dziala")
 })
 
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+//#endregion
+
 app.listen(3000, () => {
     console.log('http://localhost:3000')
 })
+
+let users = [
+    {id: 1, name: "Przemek"},
+    {id: 2, name: "Kamil"}
+]
+

61ff042a87cb114a3f6f29d7c7fa1c1cb872675a
P-Kuszyk <p.kuszyk1221@wp.pl>
2026-03-31 14:47:30 +0200

    Changes with the users endpoint

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ app.get('/users', (req, res) => {
     res.json(users)
 })
 
+app.get('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const user = users.find(u => u.id === id)
+    if(user){
+        console.log(user.name)
+        res.send(`Imię to: ${user.name}`)
+    }
+    else{
+        res.status(404).send("Nie ma takiego użytkownika")
+    }
+})
+
 //#endregion
 
 app.listen(3000, () => {
